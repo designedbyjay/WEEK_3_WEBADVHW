f79dc1dfe3b7d5768ade5db7b02f63517bf6f5e3
Jay Ramirez <ramij345@newschool.edu>
2012-10-10 11:04:19 -0400

    11:04am

--- js/main.js
+++ js/main.js
@@ -1,155 +1,192 @@
- var albums =[
-				{
-					"Name": "Idealism",
-					"Nicename": "idealism",
-					"Artist":"Digitalism",
-					"Year": "2007",
-					"Songs":[ { "name": "Magnets", "track_length": "3:50", "rating": "4 Stars" },
-							  { "name": "Zdarlight", "track_length": "6:55", "rating": "1 Stars" },
-							  { "name": "Idealistic", "track_length": "3:45", "rating": "2 Stars" },
-							  { "name": "Digitalism in Cairo", "track_length": "3:45", "rating": "2 Stars" },
-							  { "name": "Departure from Cairo", "track_length": "3:45", "rating": "2 Stars" },
-							  { "name": "Pogo", "track_length": "3:45", "rating": "2 Stars" },
-							  { "name": "Moonlight", "track_length": "3:45", "rating": "2 Stars" },
-							  { "name": "Anything New", "track_length": "3:45", "rating": "2 Stars" },
-							  { "name": "The Pulse", "track_length": "3:45", "rating": "2 Stars" },
-							  { "name": "Homezone", "track_length": "3:45", "rating": "2 Stars" },
-							  { "name": "Apollo-Gize", "track_length": "3:45", "rating": "2 Stars" },
-							  { "name": "Jupiter Approach", "track_length": "3:45", "rating": "2 Stars" },
-							  { "name": "Jupiter Room", "track_length": "3:45", "rating": "2 Stars" },
-							  { "name": "Echoes", "track_length": "3:45", "rating": "2 Stars" }
+var albums = [
+	{
+		"Name": "Idealism",
+		"Nicename": "idealism",
+		"Artist":"Digitalism",
+		"Image": "images/idealismArt.png",
+		"Year": "2007",
+		"Songs":[ 
+			{ "name": "1. Magnets", "track_length": "3:50", "rating": "4 Stars" },
+			{ "name": "2. Zdarlight", "track_length": "6:55", "rating": "1 Stars" },
+			{ "name": "3. Idealistic", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "4. Digitalism in Cairo", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "5. Departure from Cairo", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "6. Pogo", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "7. Moonlight", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "8. Anything New", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "9. The Pulse", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "10. Homezone", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "11. Apollo-Gize", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "12. Jupiter Approach", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "13. Jupiter Room", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "14. Echoes", "track_length": "3:45", "rating": "2 Stars" }
+		]
+	},
+	{
+		"Name": "Mickey Mouse Operation",
+		"Nicename": "mickey-mouse-operation",
+		"Artist":"Little People",
+		"Image": "images/mickeymouseArt.png",
+		"Year": "2006",
+		"Songs":[ 
+			{ "name": "1. Basique", "track_length": "3:45", "rating": "4 Stars" },
+			{ "name": "2. Moon", "track_length": "6:55", "rating": "1 Stars" },
+			{ "name": "3. Unsaid", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "4. Either Or", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "5. Above the clouds", "track_length": "6:55", "rating": "1 Stars" },
+			{ "name": "6. Breathe again", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "7. Intermezzo", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "8. Idiom", "track_length": "6:55", "rating": "1 Stars" },
+			{ "name": "9. Inutile et Indispensable", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "10. Last Fare", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "11. Fisticuffs at dawn", "track_length": "5:25", "rating": "3 Stars" },
+			{ "name": "12. Behind closed doors edit", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "13. Start Shootin'", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "14. Gravitas", "track_length": "3:45", "rating": "2 Stars" }
+		]
+	},
+	{
+		"Name": "Black Sands",
+		"Nicename": "black-sands-bonobo",
+		"Artist":"Bonobo",
+		"Image": "images/blacksandsArt.png",
+		"Year": "2010",
+		"Songs":[ 
+			{ "name": "1. Prelude", "track_length": "3:50", "rating": "4 Stars" },
+			{ "name": "2. Kiara", "track_length": "6:55", "rating": "1 Stars" },
+			{ "name": "3. Kong", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "4. Eyesdown", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "5. El Toro", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "6. We Could Forever", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "7. 1009", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "8. All in Forms", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "9. The Keeper", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "10. Stay the Same", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "11. Animals", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "12. Black Sands", "track_length": "3:45", "rating": "2 Stars" },
+		]
+	},
+	{
+		"Name": "Endtroducing",
+		"Nicename": "endtroducing-djshadow",
+		"Artist":"DJ Shadow",
+		"Image": "images/endtroducingArt.png",
+		"Year": "1996",
+		"Songs":[ 
+			{ "name": "1. Best Foot Forward", "track_length": "3:50", "rating": "4 Stars" },
+			{ "name": "2. Building Steam With A Grain Of Salt", "track_length": "6:55", "rating": "1 Stars" },
+			{ "name": "3. The Number Song", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "4. Changeling", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "5. Transmission", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "6. What Does Your Soul Look Like (Part 2) ", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "7. Untitled", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "8. Stem/Long Stem", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "9. Transmission 2", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "10. Mutual Slump", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "11. Why Hip Hop Sucks In '96", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "12. Midnight In A Perfect World", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "13. Napalm Brain/Scatter Brain", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "14. What Does Your Soul Look Like (Part 1 - Blue Sky Revisit)", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "15. Transmission 3", "track_length": "3:45", "rating": "2 Stars" },
+		]
+	},
+	{
+		"Name": "Destroyed",
+		"Nicename": "destroyed-moby",
+		"Artist":"Moby",
+		"Image": "images/destroyedArt.png",
+		"Year": "2011",
+		"Songs":[ 
+			{ "name": "1. The Broken Places", "track_length": "3:50", "rating": "4 Stars" },
+			{ "name": "2. Be the One", "track_length": "6:55", "rating": "1 Stars" },
+			{ "name": "3. Sevastopol", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "4. The Low Hum", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "5. Rockets", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "6. The Day", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "7. Lie Down in Darkness", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "8. Victoria Lucas", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "9. After", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "10. Blue Moon", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "11. The Right Thing", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "12. Stella Maris", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "13. The Violent Bear It Away", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "14. Lacrimae", "track_length": "3:45", "rating": "2 Stars" },
+			{ "name": "15. When You Are Old", "track_length": "3:45", "rating": "2 Stars" },
+		]
+	},
+];
 
-							]
-					},
+
+
+
+$(document).ready(function() {
+
+
+	for (var i =0; i< albums.length; i++) {
+		$('#ui_element ul').append('<li class="album '+albums[i].Nicename+'"><a href="#" rel="' + i + '">'+albums[i].Name+'</a></li>');
+		
+	}
+	
+	$('#ui_element ul li a, span.prev, span.next').live('click', function() {
+		var album_id = parseInt($(this).attr('rel'));
+		var current_album = null
+
+	// find album and empty out more info container
+
+		if (typeof albums[album_id] != 'undefined') {
+			current_album = albums[album_id];
+			$('#moreInfo').empty();
+			
+			// Artist & Album info displayed
+
+				$('#moreInfo').append('<img src="' + current_album.Image + '" class="album_img" />');
 					
-					{
-						"Name": "Mickey Mouse Operation",
-						"Nicename": "mickey-mouse-operation",
-						"Artist":"Little_People",
-						"Year": "2006",
-						"Songs":[ { "name": "Basique", "track_length": "3:45", "rating": "4 Stars" },
-								  { "name": "Moon", "track_length": "6:55", "rating": "1 Stars" },
-								  { "name": "Unsaid", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Either Or", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Above the clouds", "track_length": "6:55", "rating": "1 Stars" },
-								  { "name": "Breathe again", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Intermezzo", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Idiom", "track_length": "6:55", "rating": "1 Stars" },
-								  { "name": "Inutile et Indispensable", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Last Fare", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Fisticuffs at dawn", "track_length": "5:25", "rating": "3 Stars" },
-								  { "name": "Behind closed doors edit", "track_length": "3:45", "rating": "2 Stars" },
-  								  { "name": "Start Shootin'", "track_length": "3:45", "rating": "2 Stars" },
-  								  { "name": "Gravitas", "track_length": "3:45", "rating": "2 Stars" }
-									]
-					},
 
-					{
-						"Name": "Black Sands",
-						"Nicename": "black-sands-bonobo",
-						"Artist":"Bonobo",
-						"Year": "2010",
-						"Songs":[ { "name": "Prelude", "track_length": "3:50", "rating": "4 Stars" },
-								  { "name": "Kiara", "track_length": "6:55", "rating": "1 Stars" },
-								  { "name": "Kong", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Eyesdown", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "El Toro", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "We Could Forever", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "1009", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "All in Forms", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "The Keeper", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Stay the Same", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Animals", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Black Sands", "track_length": "3:45", "rating": "2 Stars" },
-								 
+			// meta data to go inside container with songs.
+			//$('moreInfo').append('<p class="artistName"><strong>Artist  </strong></p> '+ current_album.Artist);
+			//$('moreInfo').append('<p class="albumName"><strong>Artist </strong></p> '+ current_album.Name);
 
-									]
-					},
-
-
-					{
-						"Name": "Endtroducing",
-						"Nicename": "endtroducing-djshadow",
-						"Artist":"DJ_Shadow",
-						"Year": "1996",
-						"Songs":[ { "name": "Best Foot Forward", "track_length": "3:50", "rating": "4 Stars" },
-								  { "name": "Building Steam With A Grain Of Salt", "track_length": "6:55", "rating": "1 Stars" },
-								  { "name": "The Number Song", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Changeling", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Transmission", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "What Does Your Soul Look Like (Part 2) ", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Untitled", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Stem/Long Stem", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Transmission 2", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Mutual Slump", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Why Hip Hop Sucks In '96", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Midnight In A Perfect World", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Napalm Brain/Scatter Brain", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "What Does Your Soul Look Like (Part 1 - Blue Sky Revisit)", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Transmission 3", "track_length": "3:45", "rating": "2 Stars" },
-								 
-									]
-					},
-					{
-						"Name": "Destroyed",
-						"Nicename": "destroyed-moby",
-						"Artist":"Moby",
-						"Year": "2011",
-						"Songs":[ { "name": "The Broken Places", "track_length": "3:50", "rating": "4 Stars" },
-								  { "name": "Be the One", "track_length": "6:55", "rating": "1 Stars" },
-								  { "name": "Sevastopol", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "The Low Hum", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Rockets", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "The Day", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Lie Down in Darkness", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Victoria Lucas", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "After", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Blue Moon", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "The Right Thing", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Stella Maris", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "The Violent Bear It Away", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "Lacrimae", "track_length": "3:45", "rating": "2 Stars" },
-								  { "name": "When You Are Old", "track_length": "3:45", "rating": "2 Stars" },
-								 
-									]
-					},
-
-
-
-			];
+					$('#moreInfo').append('  <p class="extra"><strong>ALBUM </strong></p> ' + current_album.Name);
+					$('#moreInfo').append('  <p class="extra"><strong>ARTIST </strong></p> ' + current_album.Artist);
+					$('#moreInfo').append('  <p class="extra"><strong>RELEASED </strong></p> ' + current_album.Year);
 			
-
-
-
-			$(document).ready(function(){
-
-			
-				for(var i =0; i< albums.length; i++){
 				
-					$('#ui_element ul').append('<li class="album-'+albums[i].Nicename+'"></li>'); //wrapper container is created
-					$('.album-'+albums[i].Nicename).append('<li id="artistname-'+albums[i].Artist+'">'+albums[i].Artist+'</li>');	 //here goes the name of the artist
-					$('.album-'+albums[i].Nicename).append('<li id="albumYear">('+albums[i].Year+')</li>'); 			// here goes the year of release
-					$('.album-'+albums[i].Nicename).prepend('<a href="#"><li id="albumName '+albums[i].Name+'">'+albums[i].Name+' </li></a>'); 	// album name
-					$('.album-'+albums[i].Nicename).append('<p id="more-info album-'+albums[i].Nicename+'"></p>'); 			// here goes the year of release
 
-
-					  $("p#moreInfo").click(function () {
-											      $(this).slideUp();
-											    });
-					//for(var f=0; f< albums[i].Songs.length; f++)
-
-					{
-						//var text = '<li><p>'+albums[i].Songs[f].name+'</p><p>'+albums[i].Songs[f].track_length+'</p><p>'+albums[i].Songs[f].rating+'</p></li>'; //we'll show and hide this content
-						//$('p.album-'+albums[i].Nicename).append(text);
+			//Add Songs here
+			$('#moreInfo').append('<ul id="songs"/>');
+			for (var i = 0; i < current_album.Songs.length; i++) {
+				$('#songs').append('<li>' + current_album.Songs[i].name + '</li>');
+			}
+			
+			// Add prev and next tag
+			if (typeof albums[album_id - 1] != 'undefined') {
+				$('#songs').parent().append('<span class="prev" rel="' + (album_id - 1) + '">Prev</span>');
+			}
+			if (typeof albums[album_id + 1] != 'undefined') {
+				$('#songs').parent().append('<span class="next" rel="' + (album_id + 1) + '">Next</span>');
 					}
 
-				}
-					
-				
-			
-			});
+		//$('#ui_element ul').append('<li class="album-'+albums[i].Nicename+'"></li>'); //wrapper container is created
+		//$('.album-'+albums[i].Nicename).append('<li id="artistname-'+albums[i].Artist+'">'+albums[i].Artist+'</li>');	 //here goes the name of the artist
+		//$('.album-'+albums[i].Nicename).append('<li id="albumYear">('+albums[i].Year+')</li>'); 			// here goes the year of release
+		//$('.album-'+albums[i].Nicename).prepend('<a href="#"><li id="albumName '+albums[i].Name+'">'+albums[i].Name+' </li></a>'); 	// album name
+		//$('.album-'+albums[i].Nicename).append('<p id="more-info album-'+albums[i].Nicename+'"></p>'); 			// here goes the year of release
+		
+		
+		//for(var f=0; f< albums[i].Songs.length; f++)
+		
+		//{
+		//var text = '<li><p>'+albums[i].Songs[f].name+'</p><p>'+albums[i].Songs[f].track_length+'</p><p>'+albums[i].Songs[f].rating+'</p></li>'; //we'll show and hide this content
+		//$('p.album-'+albums[i].Nicename).append(text);
+		//}
 
 
-    
-					
-	
+
+		}
+	});
+
+});
+
+
+
+
